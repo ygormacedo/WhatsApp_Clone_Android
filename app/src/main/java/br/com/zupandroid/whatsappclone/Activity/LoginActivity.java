b649d78e2a14855e6e8ed85d5f9664415c18e17b
Ygor.Macedo <ygormacedo44@gmail.com>
2019-09-19 17:45:26 -0300

    Ultima etapa antes de mudar o layout de login, pois é necessario uma estretura maior como um servidor para gerar um token online -> receber -> confirmar e fazer o login.
    **** CODIGO VAI MUDAR DAQUI PRA FRENTE SE TORNANDO E-MAIL E SENHA *****

--- app/src/main/java/br/com/zupandroid/whatsappclone/Activity/LoginActivity.java
+++ app/src/main/java/br/com/zupandroid/whatsappclone/Activity/LoginActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
@@ -90,6 +92,18 @@ public class LoginActivity extends AppCompatActivity {
                 // Send to SMS.(+5511947578682)
                 telefoneSemFormatacao = "5554";
                 boolean enviadoSms = enviaSms("+" + telefoneSemFormatacao, mensagemEnvio);
+
+                if ( enviadoSms ){
+
+                    Intent intente = new Intent(LoginActivity.this, ValidadorActivity.class);
+                    startActivity(intente);
+                    finish();
+
+                }else{
+
+               Toast.makeText(LoginActivity.this,"Problema ao enviar o SMS, tente novamente", Toast.LENGTH_SHORT).show();
+
+                }
 
 //                HashMap<String, String> usuario = preferencias.getDadosUsuario();
 //                Log.e(" TOKEN", "  NOME:" +usuario.get("nome") + " FONE:" +usuario.get("telefone"));
