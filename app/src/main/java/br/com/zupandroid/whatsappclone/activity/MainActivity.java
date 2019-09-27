130f2aed1b9969ee27290160a9c0c29e7d53c645
Ygor.Macedo <ygormacedo44@gmail.com>
2019-09-26 22:57:28 -0300

    Fragment and Sliding Tab/Stip Layout

--- app/src/main/java/br/com/zupandroid/whatsappclone/activity/MainActivity.java
+++ app/src/main/java/br/com/zupandroid/whatsappclone/activity/MainActivity.java
@@ -1,19 +1,34 @@
 package br.com.zupandroid.whatsappclone.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import br.com.zupandroid.whatsappclone.R;
+import br.com.zupandroid.whatsappclone.config.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth userAuth;
+    private Toolbar toolbar1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userAuth = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        toolbar1 = findViewById(R.id.menuToolbar);
+        toolbar1.setTitle("WhatsApp");
+
 
         setSupportActionBar((Toolbar) findViewById(R.id.menuToolbar));
     }
@@ -21,7 +36,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main,menu);
+        inflater.inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.item_Sair:
+                deslogarUser();
+                return true;
+            case R.id.item_Settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void deslogarUser(){
+
+        userAuth.signOut();
+        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
---
+++ app/src/main/java/br/com/zupandroid/whatsappclone/fragment/ContatosFragment.java
@@ -0,0 +1,31 @@
+package br.com.zupandroid.whatsappclone.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import br.com.zupandroid.whatsappclone.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContatosFragment extends Fragment {
+
+
+    public ContatosFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contatos, container, false);
+    }
+
+}
