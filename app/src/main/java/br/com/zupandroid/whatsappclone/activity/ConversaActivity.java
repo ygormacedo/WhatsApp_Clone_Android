0eec3c47735f69e2beec12e9c5ed84700ed70a13
Ygor.Macedo <ygormacedo44@gmail.com>
2019-09-27 22:48:17 -0300

    Back Button TalkActivity

--- app/src/main/java/br/com/zupandroid/whatsappclone/activity/ConversaActivity.java
+++ app/src/main/java/br/com/zupandroid/whatsappclone/activity/ConversaActivity.java
@@ -3,14 +3,22 @@ package br.com.zupandroid.whatsappclone.activity;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import br.com.zupandroid.whatsappclone.R;
 
 public class ConversaActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversa);
+
+        toolbar = findViewById(R.id.toolbarConversa);
+        toolbar.setTitle("USUARIO DE DROGAS");
+        toolbar.setNavigationIcon(R.drawable.ic_action_arrow_left);
+        setSupportActionBar(toolbar);
     }
 }
