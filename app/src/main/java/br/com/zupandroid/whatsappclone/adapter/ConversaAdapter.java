a66315b8e1b118a661ff50228b743e50ea9e11fa
Ygor.Macedo <ygormacedo44@gmail.com>
2019-09-30 14:17:41 -0300

    Conversas sendo listadas na fragmentConversas

--- app/src/main/java/br/com/zupandroid/whatsappclone/adapter/ConversaAdapter.java
+++ app/src/main/java/br/com/zupandroid/whatsappclone/adapter/ConversaAdapter.java
@@ -1,4 +1,55 @@
 package br.com.zupandroid.whatsappclone.adapter;
 
-public class ConversaAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+import br.com.zupandroid.whatsappclone.R;
+import br.com.zupandroid.whatsappclone.model.Conversa;
+
+public class ConversaAdapter extends ArrayAdapter<Conversa> {
+
+    private ArrayList<Conversa> conversas;
+    private Context context;
+
+    public ConversaAdapter(@NonNull Context c, @NonNull ArrayList<Conversa> objects) {
+        super(c,0, objects);
+        this.context = c;
+        this.conversas = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = null;
+
+        //Verifica se a lista está preenchida
+
+        if (conversas != null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+
+            // montar view a partir do xml
+            view = inflater.inflate(R.layout.lista_conversas,parent,false);
+
+            //recuperar elementos para exibição na tela
+            TextView nome = view.findViewById(R.id.tituloId);
+            TextView ultimaMensagem = view.findViewById(R.id.subTituloId);
+
+            Conversa conversa = conversas.get(position);
+            nome.setText(conversa.getNome());
+            ultimaMensagem.setText(conversa.getMensagem());
+
+        }
+
+        return view;
+    }
 }
--- app/src/main/java/br/com/zupandroid/whatsappclone/fragment/ConversasFragment.java
+++ app/src/main/java/br/com/zupandroid/whatsappclone/fragment/ConversasFragment.java
@@ -5,15 +5,36 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 import br.com.zupandroid.whatsappclone.R;
+import br.com.zupandroid.whatsappclone.adapter.ConversaAdapter;
+import br.com.zupandroid.whatsappclone.config.ConfiguracaoFirebase;
+import br.com.zupandroid.whatsappclone.helper.Preferencias;
+import br.com.zupandroid.whatsappclone.model.Conversa;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ConversasFragment extends Fragment {
+
+    private ListView listView;
+    private ArrayAdapter<Conversa> adapter;
+    private ArrayList<Conversa> conversas;
+
+    private DatabaseReference firebase;
+    private ValueEventListener valueEventListenerConversas;
 
 
     public ConversasFragment() {
@@ -25,7 +46,54 @@ public class ConversasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_conversas, container, false);
+        View view = inflater.inflate(R.layout.fragment_conversas, container, false);
+
+        // Monta listView e adapter;
+        conversas = new ArrayList<>();
+        listView = view.findViewById(R.id.id_Conversas);
+        adapter = new ConversaAdapter(getActivity(), conversas);
+        listView.setAdapter(adapter);
+
+        // recuperar dados do usuario
+        Preferencias preferencias = new Preferencias(getActivity());
+        String idUsuarioLogado = preferencias.getIdentificado();
+
+
+        // recuperar dados da mensagem
+        firebase = ConfiguracaoFirebase.getFirebase()
+                .child("conversas")
+                .child(idUsuarioLogado);
+
+        valueEventListenerConversas = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                conversas.clear();
+                for (DataSnapshot dados: dataSnapshot.getChildren()){
+                    Conversa conversa = dados.getValue(Conversa.class);
+                    conversas.add(conversa);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+
+
+        return view;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+    firebase.addValueEventListener(valueEventListenerConversas);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        firebase.removeEventListener(valueEventListenerConversas);
+    }
 }
